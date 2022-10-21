646a7464e05fbb2fa0fe18d40b92e1bd4a9ce6a9
Evan Silverman <es4753@nyu.edu>
2022-10-21 00:03:42 -0700

    set up codegen for graphql typescript compatibility

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,42 @@
-// Initial file 
+import { readFileSync } from "fs";
+import { resolve } from "path";
+
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+import { Resolvers } from "./generated/graphql";
+
+const typeDefs = readFileSync(resolve(__dirname, "./schema.graphql"), {
+  encoding: "utf-8",
+});
+
+const resolvers: Resolvers = {
+  Query: {
+    players: () => [
+      {
+        name: "Pass Forfeit",
+        height: 100,
+        weight: 200,
+      },
+      {
+        name: "Nicolaj Muller",
+        height: 50,
+        weight: 2020,
+      },
+    ],
+  },
+};
+
+async function startApolloServer() {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+  });
+  console.log(`🚀  Server ready at: ${url}`);
+}
+
+startApolloServer();
