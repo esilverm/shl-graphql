5c9abc8bacf818ab125c478ba102ab47bd6aa8cb
Evan Silverman <es4753@nyu.edu>
2022-11-16 15:53:30 -0500

    implement league dataloader and rework types

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,15 +1,17 @@
-import { QueryResolvers } from "../generated/graphql";
+import { Conference, League } from "../db/models";
 
-export const Query: QueryResolvers = {
-  leagues: async (_, __, { dataSources }) => {
-    return await dataSources.sequelize.models.League.findAll();
+export const Query = {
+  leagues: async () => {
+    return await League.findAll();
   },
-  conferences: async (_, { season, league }, { dataSources }) => {
-    return await dataSources.sequelize.models.Conference.findAll({
+  conferences: async (_, { season, league }) => {
+    const conferences = await Conference.findAll({
       where: {
         season,
         ...(league !== undefined && { league }),
       },
+      raw: true,
     });
+    return conferences;
   },
 };
