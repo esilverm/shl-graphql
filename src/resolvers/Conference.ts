9b4e1288f35c4db683ae17870030766c405dcb3b
Evan Silverman <es4753@nyu.edu>
2022-11-15 20:52:55 -0500

    cleanup

--- src/resolvers/Conference.ts
+++ src/resolvers/Conference.ts
@@ -2,7 +2,6 @@ import { ConferenceResolvers } from "../generated/graphql";
 
 export const Conference: ConferenceResolvers = {
   league: async ({ league }, _, { dataSources }) => {
-    console.log(league);
     return await dataSources.sequelize.models.League.findOne({
       where: {
         id: league,
